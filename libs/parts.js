b77fd36abe100860eb496d8d9263233f9685f3de
Tyler Morrison <tyler@counterpointstudio.com>
2016-07-08 11:29:20 -0400

    Setup webpack-dev-server and HMR

---
+++ libs/parts.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+
+exports.devServer = (options) => {
+    return {
+        devServer: {
+            // Enable history API fallback so HTML5 History API based routing works.
+            // This is a good default that will come in handy for more complicated setups.
+            historyApiFallback: true,
+
+            // Unlike the CLI flag, the following does NOT set HotModuleReplacementPlugin
+            hot: true,
+            inline: true,
+
+            // Display only errors to reduce the amount of output in console
+            stats: 'errors-only',
+
+            // Parse host and port from env to allow customization.
+            //
+            // If you use Vagrant or Cloud9, set
+            // host: options.host || '0.0.0.0';
+            //
+            // 0.0.0.0 is available to all network devices unlike default `localhost`
+            host: options.host, // Defaults to `localhost`
+            port: options.port // Defaults to 8080
+        },
+        plugins: [
+            // Enable multi-pass compilation for enhanced performance in larger projects.
+            // This is a good default.
+            new webpack.HotModuleReplacementPlugin({
+                multiStep: true
+            })
+        ]
+    };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,52 @@
 // External Dependencies
 const path = require('path');
 
+// Internal Dependencies
+const parts = require('./libs/parts');
+
 // Webpack Plugins
 const merge = require('webpack-merge'),
-  validate = require('webpack-validator'),
-  HtmlWebpackPlugin = require('html-webpack-plugin');
+    validate = require('webpack-validator'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
 };
 
 const common = {
-  //Entry accepts a path or an object of entries.
-  //We’ll be using the latter form given it’s convenient with more complex configurations.
-  entry: {
-    app: PATHS.app
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack Demo'
-    })
-  ]
+    //Entry accepts a path or an object of entries.
+    //We’ll be using the latter form given it’s convenient with more complex configurations.
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Demo'
+        })
+    ]
 };
 
 var config;
 
 // Detect how NPM is run and branch bases on that env variable
 switch (process.env.npm_lifecycle_event) {
-  case 'build':
-    config = merge(common, {})
-    break;
-  default:
-    config = merge(common, {});
+    case 'build':
+        config = merge(common, {})
+        break;
+    default:
+        config = merge(
+            common,
+            parts.devServer({
+                // Customize host/port here if needed
+                host: process.env.HOST,
+                port: process.env.PORT
+            })
+        );
 }
 
 // Adding webpack-validator to warn us of config issues
